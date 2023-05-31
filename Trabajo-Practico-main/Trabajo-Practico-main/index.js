444c19ba91f46490da3ef800b948190d02298b05
Ignacio Alcaraz <66889175+Ignaciacaraz@users.noreply.github.com>
2023-05-30 21:53:00 -0300

    Add files via upload

---
+++ Trabajo-Practico-main/Trabajo-Practico-main/index.js
@@ -0,0 +1,35 @@
+var cuerpoweb = document.body; 
+var formulario =document.querySelector("form")
+var empresa = document.querySelector("h1")
+var nombre = document.querySelectorAll("input")
+
+
+function cambiarModo() { 
+    
+    cuerpoweb.classList.toggle("oscuro"); 
+    formulario.classList.toggle("oscuro");
+    empresa.classList.toggle("oscuro");
+    for (var i = 0; i < nombre.length; i++) {
+        nombre[i].classList.toggle("oscuro");
+      }
+
+}                          
+
+
+
+
+
+function submitForm() {
+  var usuario = document.getElementById("usuario").value;
+  var password  = document.getElementById("contraseña").value;
+  if (usuario === "coordinador" && password=== "contraseña" ) {
+    formulario.action = "inicio2.html";
+  } 
+  else if (usuario === "empleado" && password=== "contraseña") {
+    formulario.action = "inicio.html";
+  }
+  else{
+    alert("El usuario o la contraseña son incorrectas, intenta nuevamente");
+  }
+  formulario.submit();
+} 
